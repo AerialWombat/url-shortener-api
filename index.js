45878cf709c36fd8ef429de0680b3478f0b2b6c5
AerialWombat <adrianzhan@gmail.com>
2019-03-29 10:52:47 -0400

    Heroku commit

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ const app = express();
 const database = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "django",
-    password: "",
-    database: "url-short"
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
